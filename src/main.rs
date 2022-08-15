50f1564ce4b72edcb3ace15e2bf08d630fecabb8
Ryan Bourdais <ryanbourdais@outlook.com>
2022-08-15 07:56:51 -0500

    feat: added texture

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 #![allow(unused_variables)]
 #[macro_use]
 extern crate glium;
+extern crate image;
+
+use std::io::Cursor;
 
 fn main() {
     use glium::{glutin, Surface};
+    
 
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
+    let image = image::load(Cursor::new(&include_bytes!("../opengl.png")), image::ImageFormat::Png).unwrap().to_rgba8();
+
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
+
     #[derive(Copy, Clone)]
     struct Vertex {
         position: [f32; 2],
+        tex_coords: [f32; 2],
     }
 
-    implement_vertex!(Vertex, position);
+    implement_vertex!(Vertex, position, tex_coords);
 
-    let vertex1 = Vertex { position: [-0.5, -0.5]};
-    let vertex2 = Vertex { position: [0.0, 0.5]};
-    let vertex3 = Vertex { position: [0.5, -0.25]};
+    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0]};
+    let vertex2 = Vertex { position: [0.0, 0.5], tex_coords: [0.0, 1.0]};
+    let vertex3 = Vertex { position: [0.5, -0.25], tex_coords: [1.0, 0.0]};
     let shape = vec![vertex1, vertex2, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
@@ -30,10 +41,13 @@ fn main() {
         #version 140
 
         in vec2 position;
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
 
         uniform mat4 matrix;
         
         void main() {
+            v_tex_coords = tex_coords;
             gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
@@ -41,10 +55,13 @@ fn main() {
     let fragment_shader_src = r#"
         #version 140
         
+        in vec2 v_tex_coords;
         out vec4 color;
         
+        uniform sampler2D tex;
+
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = texture(tex, v_tex_coords);
         }
     "#;
 
@@ -87,7 +104,8 @@ fn main() {
                 [-t.sin(), t.cos(), 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
                 [t, 0.0, 0.0, 1.0f32],
-            ]
+            ],
+            tex: &texture,
         };
         target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
         target.finish().unwrap();
