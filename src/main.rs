26080fdd7b96849b1197ea0c5079ca5f180a8747
Ryan Bourdais <ryanbourdais@outlook.com>
2022-08-14 22:41:37 -0500

    feat: implemented 90s method of animation

--- src/main.rs
+++ src/main.rs
@@ -49,17 +49,11 @@ fn main() {
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    event_loop.run(move |ev, _, control_flow| {
+    let mut t: f32 = -0.5;
 
-        let mut target = display.draw();
+    event_loop.run(move |event, _, control_flow| {
 
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
-        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-        target.finish().unwrap();
-    
-        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-        match ev {
+        match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
@@ -67,7 +61,34 @@ fn main() {
                 },
                 _ => return,
             },
-            _ => (),
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
         }
+
+        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        //update t
+        t += 0.002;
+        if t > 0.5 {
+            t = -0.5
+        }
+
+        let vertex1 = Vertex { position: [-0.5 + t, -0.5] };
+        let vertex2 = Vertex { position: [ 0.0 + t,  0.5] };
+        let vertex3 = Vertex { position: [ 0.5 + t, -0.25] };
+        let shape = vec![vertex1, vertex2, vertex3];
+
+        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                &Default::default()).unwrap();
+        target.finish().unwrap();
     });
 }
