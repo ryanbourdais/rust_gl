431b8cf6d5f8fc06cb8ad2bc3fcb2a77bcbf3393
Ryan Bourdais <ryanbourdais@outlook.com>
2022-08-15 21:37:10 -0500

    feat: rendered teapot

--- src/main.rs
+++ src/main.rs
@@ -15,54 +15,30 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let image = image::load(Cursor::new(&include_bytes!("../opengl.png")), image::ImageFormat::Png).unwrap().to_rgba8();
-
-    let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
-
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-        tex_coords: [f32; 2],
-    }
-
-    implement_vertex!(Vertex, position, tex_coords);
-
-    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0]};
-    let vertex2 = Vertex { position: [0.0, 0.5], tex_coords: [0.0, 1.0]};
-    let vertex3 = Vertex { position: [0.5, -0.25], tex_coords: [1.0, 0.0]};
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &teapot::INDICES).unwrap();
 
     let vertex_shader_src = r#"
         #version 140
 
-        in vec2 position;
-        in vec2 tex_coords;
-        out vec2 v_tex_coords;
+        in vec3 position;
+        in vec3 normal;
 
         uniform mat4 matrix;
         
         void main() {
-            v_tex_coords = tex_coords;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
+            gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
         
-        in vec2 v_tex_coords;
         out vec4 color;
-        
-        uniform sampler2D tex;
 
         void main() {
-            color = texture(tex, v_tex_coords);
+            color = vec4(1.0, 0.0, 0.0, 1.0);
         }
     "#;
 
@@ -99,16 +75,16 @@ fn main() {
         let mut target = display.draw();
 
         target.clear_color(0.0, 0.0, 1.0, 1.0);
-        let uniforms = uniform! {
-            matrix: [
-                [t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [t, 0.0, 0.0, 1.0f32],
-            ],
-            tex: &texture,
-        };
-        target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        
+        let matrix = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 0.0, 1.0f32]
+        ];
+
+        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix }, &Default::default()).unwrap();
+
         target.finish().unwrap();
 
     });
