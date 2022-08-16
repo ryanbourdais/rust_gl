5a1df63e31e68e092a43952add1b39bacf03162f
Ryan Bourdais <ryanbourdais@outlook.com>
2022-08-15 22:05:00 -0500

    feat: added normal map and depth

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
     let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
@@ -20,25 +20,35 @@ fn main() {
     let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &teapot::INDICES).unwrap();
 
     let vertex_shader_src = r#"
-        #version 140
+        #version 150
 
         in vec3 position;
         in vec3 normal;
 
+        out vec3 v_normal;
+
         uniform mat4 matrix;
         
         void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
             gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
-        #version 140
+        #version 150
         
+        in vec3 v_normal;
+
         out vec4 color;
 
+        uniform vec3 u_light;
+
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
 
@@ -74,7 +84,7 @@ fn main() {
 
         let mut target = display.draw();
 
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
         
         let matrix = [
             [0.01, 0.0, 0.0, 0.0],
@@ -83,7 +93,15 @@ fn main() {
             [0.0, 0.0, 0.0, 1.0f32]
         ];
 
-        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix }, &Default::default()).unwrap();
+        let light = [-1.0, 0.4, 0.9f32];
+
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess, write: true, ..Default::default() },
+                ..Default::default()
+            };
+
+        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, u_light: light }, &params).unwrap();
 
         target.finish().unwrap();
 
